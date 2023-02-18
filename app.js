b62f568926b00fa1350a996c3a26d5978943ad29
Sebsprograms <sebastianbenad72@gmail.com>
2023-02-18 12:42:08 +0100

    work todos and ejs layouts

--- app.js
+++ app.js
@@ -8,18 +8,34 @@ app.set('view engine', 'ejs');
 
 const PORT = 3000;
 
-let items = ["Sample Item"];
+let personalTodoList = ["Sample Item"];
+let workTodoList = [];
 
 app.get('/', (req, res) => {
 
     const options = { weekday: 'long', day: 'numeric', month: 'long', };
     const today = new Date().toLocaleString('en-us', options);
-    res.render('index', { day: today, newItem: items });
+    res.render('index', { pageTitle: today, todoItems: personalTodoList, buttonValue: today });
+})
+
+app.get("/work", (req, res) => {
+    const workTitle = "Work"
+    res.render('index', {pageTitle: workTitle, todoItems: workTodoList, buttonValue: workTitle});
+})
+
+app.get("/about", (req, res) => {
+    res.render('about');
 })
 
 app.post('/', (req, res) => {
-    items.push(req.body.newTodo);
-    res.redirect('/');
+    const newTodoItem = req.body.newTodo;
+    if(req.body.button === "Work") {
+        workTodoList.push(newTodoItem);
+        res.redirect('/work');
+    } else {
+        personalTodoList.push(newTodoItem);
+        res.redirect('/');
+    }
 })
 
 
